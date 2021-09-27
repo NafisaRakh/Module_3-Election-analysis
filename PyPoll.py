b1c67fd5966e69f26a901b33184768167d9913de
Nafisa <89433961+Nafisa85@users.noreply.github.com>
2021-09-26 21:34:27 -0500

    s to c

--- PyPoll.py
+++ PyPoll.py
@@ -8,19 +8,8 @@
 # Assign a variable for the file to load and the path.
 # Assign a variable for the file to load and the path.
 # Import the datetime class from the datetime module.
-import datetime
-# Use the now() attribute on the datetime class to get the present time.
-now = datetime.datetime.now()
-# Print the present time.
-print("The time right now is ", now)
-# Import the datetime class from the datetime module.
-import datetime as dt
-# Use the now() attribute on the datetime class to get the present time.
-now = dt.datetime.now()
-# Print the present time.
-print("The time right now is ", now)
 # Assign a variable for the file to load and the path.
-file_to_load = 'election_results.csv'
+file_to_load = 'Resources/election_results.csv'
 # Open the election results and read the file.
 election_data = open(file_to_load, 'r')
 
@@ -28,43 +17,3 @@ election_data = open(file_to_load, 'r')
 
 # Close the file.
 election_data.close()
-# Open the election results and read the file
-with open(file_to_load) as election_data:
-
-     # To do: perform analysis.
-     print(election_data)
-     import csv
-import os
-# Assign a variable for the file to load and the path.
-file_to_load = os.path.join("election_results.csv")
-# Open the election results and read the file.
-with open(file_to_load) as election_data:
-
-    # Print the file object.
-     print(election_data)
-     # Create a filename variable to a direct or indirect path to the file.
-file_to_save = os.path.join("election_analysis.txt")
-# Using the open() function with the "w" mode we will write data to the file.
-open(file_to_save, "w")
-# Create a filename variable to a direct or indirect path to the file.
-file_to_save = os.path.join("analysis", "election_analysis.txt")
-# Using the open() function with the "w" mode we will write data to the file.
-open(file_to_save, "w")
-# Create a filename variable to a direct or indirect path to the file.
-file_to_save = os.path.join("analysis", "election_analysis.txt")
-
-# Using the with statement open the file as a text file.
-outfile = open(file_to_save, "w")
-# Write some data to the file.
-outfile.write("Hello World")
-
-# Close the file
-outfile.close()
-# Create a filename variable to a direct or indirect path to the file.
-file_to_save = os.path.join("analysis", "election_analysis.txt")
-
-# Using the with statement open the file as a text file.
-with open(file_to_save, "w") as txt_file:
-
-    # Write some data to the file.
-    txt_file.write("Hello World")
