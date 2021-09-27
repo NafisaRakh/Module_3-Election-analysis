ba226c77c7601a487ce2237adddab276893b75cf
Nafisa <89433961+Nafisa85@users.noreply.github.com>
2021-09-26 22:32:52 -0500

    updating folders

--- PyPoll.py
+++ PyPoll.py
@@ -10,6 +10,19 @@
 # Import the datetime class from the datetime module.
 # Assign a variable for the file to load and the path.
 # Assign a variable for the file to load and the path.
+# Import the datetime class from the datetime module.
+import datetime
+# Use the now() attribute on the datetime class to get the present time.
+now = datetime.datetime.now()
+# Print the present time.
+print("The time right now is ", now)
+# Import the datetime class from the datetime module.
+import datetime as dt
+# Use the now() attribute on the datetime class to get the present time.
+now = dt.datetime.now()
+# Print the present time.
+print("The time right now is ", now)
+# Assign a variable for the file to load and the path.
 file_to_load = 'Resources/election_results.csv'
 # Open the election results and read the file.
 election_data = open(file_to_load, 'r')
@@ -42,10 +55,19 @@ file_to_save = os.path.join("analysis", "election_analysis.txt")
 # Using the with statement open the file as a text file.
 outfile = open(file_to_save, "w")
 # Write some data to the file.
-outfile.write("Arapahoe")
-outfile.write("Denver")
-outfile.write("Jefferson")
+outfile.write("Hello World")
+
 # Close the file
+outfile.close()
+# Create a filename variable to a direct or indirect path to the file.
+file_to_save = os.path.join("analysis", "election_analysis.txt")
+
+# Using the with statement open the file as a text file.
+with open(file_to_save, "w") as txt_file:
+
+    # Write some data to the file.
+     txt_file.write("Arapahoe, Denver, Jefferson")
+txt_file.close()
 # Add our dependencies.
 import csv
 import os
@@ -56,19 +78,20 @@ file_to_save = os.path.join("analysis", "election_analysis.txt")
 
 # Open the election results and read the file.
 with open(file_to_load) as election_data:
-# Read the file object with the reader function.
+
+    # To do: read and analyze the data here.
+     # Read the file object with the reader function.
     file_reader = csv.reader(election_data)
      # Print each row in the CSV file.
     for row in file_reader:
         print(row)
-         # Read the file object with the reader function.
+        # Read the file object with the reader function.
     file_reader = csv.reader(election_data)
 
     # Print the header row.
     headers = next(file_reader)
     print(headers)
-    ArapahoeDenverJefferson
-# Add our dependencies.
+    # Add our dependencies.
 import csv
 import os
 # Assign a variable to load a file from a path.
